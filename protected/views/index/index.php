ed8b08dc65793731daad0b5691844c1a025d5a5d
Boyko Igor <igosja@ukr.net>
2017-09-11 14:04:09 +0300

    +

--- protected/views/index/index.php
+++ protected/views/index/index.php
@@ -1,1 +1,138 @@
-Index page
+<section class="content">
+    <div class="main-slider" id="slider">
+        <div class="item" style="background: url(img/m-slider.jpg) center top no-repeat;">
+            <div class="wrap">
+                <img src="/img/m-banner-logo.png" alt="">
+                <div class="m-slider__title">Продукция для стоматологов из Канады</div>
+                <div class="m-slider__text">
+                    Мы поставляем нашим партнерам не просто дезинфектанты (ополаскиватели, абразивы), а УВЕРЕННОСТЬ В КАЧЕСТВЕ НАШЕЙ ПРОДУКЦИИ при производстве которой все эти "мелочи" учитываются: качество сырья, соблюдение технологии производства, тестирование эффективности производствадукции.
+                </div>
+                <div class="centered"><a href="" class="btn">Заказать продукцию</a></div>
+            </div>
+        </div>
+        <div class="item" style="background: url(img/m-slider.jpg) center top no-repeat;">
+            <div class="wrap">
+                <img src="/img/m-banner-logo.png" alt="">
+                <div class="m-slider__title">Продукция для стоматологов </div>
+                <div class="m-slider__text">
+                    Мы поставляем нашим партнерам не просто дезинфектанты (ополаскиватели, абразивы), а УВЕРЕННОСТЬ В.
+                </div>
+                <div class="centered"><a href="" class="btn">Заказать продукцию</a></div>
+            </div>
+        </div>
+
+        <div class="item" style="background: url(img/m-slider.jpg) center top no-repeat;">
+            <div class="wrap">
+                <img src="/img/m-banner-logo.png" alt="">
+                <div class="m-slider__title">Продукция</div>
+                <div class="m-slider__text">
+                    Мы поставляем нашим партнерам не просто дезинфектанты (ополаскиватели, абразивы), а УВЕРЕННОСТЬ В КАЧЕСТВЕ НАШЕЙ ПРОДУКЦИИ при производстве которой все эти "мелочи" учитываются: качество сырья.
+                </div>
+                <div class="centered"><a href="" class="btn">Заказать продукцию</a></div>
+            </div>
+        </div>
+    </div>
+    <div class="main-cat">
+        <div class="wrap">
+            <h2 class="title">Каталог товаров</h2>
+            <div class="cleerfix cats">
+                <a href="" class="cats__i">
+                    <img src="/img/cats/cat-1.png" alt="">
+                    Дентальная<br /> профилактика
+                </a><a href="" class="cats__i">
+                    <img src="/img/cats/cat-2.png" alt="">
+                    Дезинфекция<br /> систем аспирации
+                </a><a href="" class="cats__i">
+                    <img src="/img/cats/cat-3.png" alt="">
+                    Дезинфекция<br /> шлангов DUWLS
+                </a><a href="" class="cats__i">
+                    <img src="/img/cats/cat-4.png" alt="">
+                    Дезинфекция<br /> поверхностей
+                </a><a href="" class="cats__i">
+                    <img src="/img/cats/cat-5.png" alt="">
+                    Дезинфекция дентального<br /> инструментария
+                </a><a href="" class="cats__i">
+                    <img src="/img/cats/cat-6.png" alt="">
+                    Стерилизация стомато<br /> инструментария
+                </a><a href="" class="cats__i">
+                    <img src="/img/cats/cat-7.png" alt="">
+                    Очистка и дезинфекция<br /> автоклава
+                </a><a href="" class="cats__i">
+                    <img src="/img/cats/cat-8.png" alt="">
+                    Дезинфекция<br /> оттисков
+                </a><a href="" class="cats__i">
+                    <img src="/img/cats/cat-9.png" alt="">
+                    Расходные<br /> материалы
+                </a><a href="" class="cats__i">
+                    <img src="/img/cats/cat-10.png" alt="">
+                    Действующие<br /> акции
+                </a>
+            </div>
+            <div class="btn-more__out">
+                <a href="" class="btn-more">Вся продукция</a>
+            </div>
+            <div class="main-cat__text">
+                EZMEDIX отслеживает  тренды мировой стоматологии и следуя им СОЗДАЕТ СОБСТВЕННЫЕ эффективные композиции.<br />
+                Купить стоматологические материалы EZMEDIX можно по доступной цене и в удобной упаковке.
+            </div>
+        </div>
+    </div>
+    <div class="main-b">
+        <div class="wrap">
+            <div class="main-b__in">
+                <div class="main-b__title">
+                    EZMEDIX — производитель продуктов<br />
+                    регулярного использования<br />
+                    в дентальной медицинской практике для:
+                </div>
+                <ul>
+                    <li><span>ДЕЗИНФЕКЦИИ</span> (инструментария, поверхностей, рук)</li>
+                    <li><span>ХОЛОДНОЙ СТЕРИЛИЗАЦИИ</span></li>
+                    <li><span>ПРОФИЛАКТИКТИЧЕСКОГО</span> и <span>ГИГИЕНИЧЕСКОГО УХОДА</span></li>
+                    <li><span>КЛИНИНГА</span>, включая очистку от микробных биопленок (microbial biofilms), присутствующих в шлангах систем водоснабжения стоматологических установок (DUWL)</li>
+                </ul>
+                <p>Современная стоматология — высокотехнологичная и высоко-конкурентная отрасль медицины. Отрасль, в которой не может быть «мелочей»: ни в предоставляемых услугах,<br /> ни в расходуемых материалах.</p>
+                <p>Мы проводим: микробиологические, вирусологические, токсикологические исследования которыми готовы с вами поделиться.</p>
+            </div>
+        </div>
+    </div>
+    <div class="main-news">
+        <div class="wrap clearfix">
+            <h2 class="title">Новости и акции:</h2>
+            <div class="clearfix news">
+                <a href="index-news-one.html" class="news__i">
+                    <img src="img/trash/news-1.jpg" alt="" class="news__i__img">
+                    <span class="news__i__date">01.06.2017</span>
+                    <span class="news__i__title">Семинар «Микробные биопленки, как фактор конкурентоспособности »</span>
+                    <span class="news__i__text">Want create site? With Free visual composer you can do it easy.  Семинар   «Микробные биопленки, как фактор конкурентоспособности »  в г . Тернополь   ( 6 апреля) 13 00 </span>
+                    <span class="news__i__link">Подробнее</span>
+                </a>
+                <a href="index-news-one.html" class="news__i">
+                    <img src="img/trash/news-2.jpg" alt="" class="news__i__img">
+                    <span class="news__i__date">01.06.2017</span>
+                    <span class="news__i__title">Разрушители биопленок  от EZMEDIX  AQUAStab и  AQUAStab+</span>
+                    <span class="news__i__text">Want create site? With Free visual composer you can do it easy.  Семинар   «Микробные биопленки, как фактор конкурентоспособности »  в г . Тернополь   ( 6 апреля) 13 00</span>
+                    <span class="news__i__link">Подробнее</span>
+                </a>
+                <a href="index-news-one.html" class="news__i">
+                    <img src="img/trash/news-3.jpg" alt="" class="news__i__img">
+                    <span class="news__i__date">01.06.2017</span>
+                    <span class="news__i__title">Специализированные очистители  от микробных биопленок в 2016 году отметили 15-летие</span>
+                    <span class="news__i__text">Want create site? With Free visual composer you can do it easy.  Семинар   «Микробные биопленки, как фактор конкурентоспособности »  в г . Тернополь   ( 6 апреля) 13 00</span>
+                    <span class="news__i__link">Подробнее</span>
+                </a>
+            </div>
+            <div class="news-more">
+                <a href="" class="btn-more">Все статьи</a>
+            </div>
+        </div>
+    </div>
+    <div class="gr-text">
+        <div class="wrap">
+            <h3 class="gr-text__title">EZMEDIX — производитель продуктов регулярного использования в дентальной медицинской практике для:</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio.</p>
+            <p>Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo.</p>
+            <p>Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio.</p>
+        </div>
+    </div>
+</section>
