1287dcd75cd335942cdeb7d3575f6408d8a4e542
Boyko Igor <igosja@ukr.net>
2017-11-17 13:49:10 +0200

    +

--- protected/views/catalog/item.php
+++ protected/views/catalog/item.php
@@ -3,26 +3,34 @@
  * @var $item Product
  */
 ?>
-<?= CHtml::link(
-    '<span class="cat__i__img">
-        <img
+<div class="cat__i">
+    <?= CHtml::link(
+        '<img
             src="' . ImageIgosja::resize(isset($item['image'][0]) ? $item['image'][0]['image_id'] : 0, 280, 280) . '"
             alt="' . $item['h1_' . Yii::app()->language] . '"
-        />
-    </span>
+        />',
+        array('product/view', 'id' => $item['url']),
+        array('class' => 'cat__i__img')
+    ); ?>
     <span class="cat__i__title">
-    <span>
-    ' . $item['h1_' . Yii::app()->language] . '
-    </span>
+        <span>
+            <?= $item['h1_' . Yii::app()->language]; ?>
+        </span>
     </span>
     <span class="cat__i__text">
-    ' . mb_substr(strip_tags($item['text_' . Yii::app()->language]), 0, 120) . '
+        <?= mb_substr(strip_tags($item['text_' . Yii::app()->language]), 0, 120); ?>
     </span>
-    <span class="cat__i__bot">
-    <span class="cat__i__link">
-    ' . Yii::t('views.catalog.index', 'link-detail') . '
+    <span class="cat__i__bot clearfix">
+        <span class="cat__i__price"><?= Yii::app()->numberFormatter->formatDecimal($item['price']); ?> грн.</span>
+        <?= CHtml::link(
+            '',
+            array('profile/product'),
+            array('class' => 'cat__i__basket')
+        ); ?>
+        <?= CHtml::link(
+            Yii::t('views.catalog.index', 'link-detail'),
+            array('product/view', 'id' => $item['url']),
+            array('class' => 'cat__i__link')
+        ); ?>
     </span>
-    </span>',
-    array('product/view', 'id' => $item['url']),
-    array('class' => 'cat__i')
-); ?>
+</div>
